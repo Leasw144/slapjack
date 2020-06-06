96a427e069734910ac88757f114a7d842f7f4f7e
Sean Leas <phoenix_24leas.com>
2020-06-06 14:18:20 -0600

    Instantiate game, and players in main js, create untested deal Cards function

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-window.addEventListener(`onload`, deal)
+var game = new Game();
+var player1 = new Player(1);
+var player2 = new Player(2)
+
+
+window.addEventListener(`onload`, dealCards)
+
+
+
+function dealCards() {
+  game.shuffleDeck();
+  game.deal()
+}
