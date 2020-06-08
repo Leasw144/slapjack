07279b3ac0cac7d13fb1679ea4a69abe4030b124
Sean Leas <phoenix_24leas.com>
2020-06-07 22:30:17 -0600

    Add psuedo code to keyCode handler

--- main.js
+++ main.js
@@ -18,12 +18,34 @@ function distributeCards() {
 
 function playerDeal() {
   if (event.keyCode == 81 && game.currentPlayer === player1) {
+    //when a player deals, this will happen 
+    // // element leaves player hand and goes to pile.
+    // // pile displays this card
+    // // //this will happen by changing the src
+    // // //this function will disable
     console.log(`you pressed Q`)
   } else if (event.keyCode == 70 && game.currentPlayer === player1) {
+    // When a player slaps, this will happen
+    // // the game will check for a set win
+    // // the game will check for a match win
+    // // if both set and match return false, player forfeits their card
+    // // show alert that you done messed up
+    // // swap player
     console.log(`you pressed F`)
   } else if (event.keyCode == 80 && game.currentPlayer === player2 ) {
+    //when a player deals, this will happen 
+    // // element leaves player hand and goes to pile.
+    // // pile displays this card
+    // // //this will happen by changing the src
+    // // //this function will disable
     console.log(`you pressed P`)
   } else if (event.keyCode == 74 && game.currentPlayer === player2) {
+     // When a player slaps, this will happen
+    // // the game will check for a set win
+    // // the game will check for a match win
+    // // if both set and match return false, player forfeits their card
+    // // show alert that you done messed up
+    // // swap player
     console.log(`you pressed J`)
   }
 }
